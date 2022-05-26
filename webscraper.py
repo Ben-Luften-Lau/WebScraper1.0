724d5d98728afc75357560254b6de133ba710e74
Ben-Luften-Lau <106262664+Ben-Luften-Lau@users.noreply.github.com>
2022-05-26 23:54:02 +0800

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,18 +1,42 @@
-import selenium
-
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 print("Loading")
 
-from selenium import webdriver
-url = '##Input URL##'
+#   Replace: ### with correct data
+url = '###Replace url '
 driver = webdriver.Firefox(
-    executable_path=r'/##InputWeb Driver File Path##'
+    executable_path=r'###'
 )
 driver.get(url)
 driver.maximize_window()
+#   Find 2 items
+#  Listingprice
+Listingprice = driver.find_elements(
+    by=By.XPATH, value="###")
+Listingprice_list = []
 
-Price = driver.find_element_by_xpath('##Xpath Location')
-matches = driver.find_elements_by_tag_name('li')
-for match in matches:
-    print (match.text)
+for Lprice in range(len(Listingprice)):
+    Listingprice_list.append(Listingprice[Lprice].text)
 
-#confirm works !!!!!
+
+#   Price per square feet
+PricePSqft = driver.find_elements(
+    by=By.XPATH, value='###')
+PricePSqft_list = []
+
+for L_PricePSqft in range(len(PricePSqft)):
+    PricePSqft_list.append(PricePSqft[L_PricePSqft].text)
+
+#   working well 25 May 2022 Wed 3:11PM
+#   get rid of () Price per sq feet data
+
+dList = list(zip(Listingprice_list, PricePSqft_list))
+List = pd.DataFrame(dList, columns=['Listing Price', 'Listing Price Per SQFeet'])
+List.to_csv('Test.csv', index=False)
+
+#   Mission Complete
+#   Working well as planned 26 May 2022 THU 12:28 AM
+driver.quit()
+print("END")
+exit('')
